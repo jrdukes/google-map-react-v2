a266f54b6a420718ac73624c739d120529f60d52
Joe Dukes <joedukes.email@gmail.com>
2019-08-06 14:11:44 -0400

    update map.js

--- React.js/map.js
+++ React.js/map.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
-import GoogleMapReact from 'google-map-react'
-const AnyReactComponent = ({ text }) => <div>{ text }</div>;
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 export default class Map extends Component {
   static defaultProps = {
-    center: { lat: 40.7446790, lng: -73.9485420 },
+    center: { lat: 40.744679, lng: -73.948542 },
     zoom: 11
-  }
-render() {
+  };
+  render() {
     return (
       <div className='google-map'>
         <GoogleMapReact
-          defaultCenter={ this.props.center }
-          defaultZoom={ this.props.zoom }>
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
           <AnyReactComponent
-            lat={ 40.7473310 }
-            lng={ -73.8517440 }
-            text={ 'Where's Waldo?' }
+            lat={40.747331}
+            lng={-73.851744}
+            text={'Where is this?'}
           />
         </GoogleMapReact>
       </div>
-    )
+    );
   }
 }
